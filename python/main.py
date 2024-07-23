a7909120f70c543715fc5184533ba6adc4d37a99
~AB <srivastava07.abhishek@gmail.com>
2024-07-23 20:32:05 +0530

    new changes

--- python/main.py
+++ python/main.py
@@ -64,8 +64,8 @@ def getOperationSet():
     print("")
 
     while True:
-        prompt = input("Choose an option (1 or 2 or 5(Data will be deleted)): ")
-        if str(prompt) == '1' or str(prompt) == '2' or str(prompt) == '5':
+        prompt = input("Choose an option between 1-6 ('5' means Data will be deleted)): ")
+        if str(prompt) == '1' or str(prompt) == '2' or str(prompt) == '3' or str(prompt) == '4' or str(prompt) == '5' or str(prompt) == '6':
             return prompt
         else:
             logger.error("\n")
@@ -73,37 +73,86 @@ def getOperationSet():
 
 
 def main():
+    output=[]
     print("The application measures the Redis memory optimisation")
     time.sleep(1)
     selectedOps = getOperationSet()
 
+    # First loads raw data
+    # Calculates memory & Time taken
+    # Then loads compressed data
+    # Calculates memory & Time taken
+    # Calculates the difference
     if int(selectedOps) == 1:
+        startTime = time.time()
         Dataloader(conn).generate(pattern="_SAM_:", commands=commands)
-        # Check total memory usage after the operation
+        endTime = time.time()
+        output.append(f"Time taken to ingest raw data {(endTime - startTime):.3f} seconds")
+        print(f"Time taken to ingest raw data {(endTime - startTime):.3f} seconds")
         before = calculate_memory_usage()
-        logger.info(f'Memory usage after loading uncompressed data: {before:.2f} MB')
-        DataReader(conn).read(pattern="_SAM_:")
-        time.sleep(0.5)
-        prompt = input("Do you want to measure the memory for compressed data? (y/n)")
-        if prompt == 'y':
-            conn.flushdb()
-            Dataloader(conn).generateAndCompress(pattern="_SAM_:", commands=commands)
-            # Check total memory usage after the operation
-            after = calculate_memory_usage()
-            logger.info(f'Memory usage after compression: {after:.2f} MB')
+        output.append(f'Memory usage after loading raw data: {before:.2f} MB')
+        print(f'Memory usage after loading raw data: {before:.2f} MB')
 
-            logger.info(f'Actual dataset usage of compressed data: {before - after:.2f} MB')
-            logger.info(f'Optimization achieved: {(before - after) * 100 / before:.2f}%')
-            DataReader(conn).readAndDecompress(pattern="_SAM_:")
-    elif int(selectedOps) == 2:
-        Dataloader(conn).generateAndCompress(pattern="_SAM_:", commands=commands)
-        # Check total memory usage after the operation
-        after = calculate_memory_usage()
-        logger.info(f'Memory usage with compression: {after:.2f} MB')
-        DataReader(conn).readAndDecompress(pattern="_SAM_:")
-    elif int(selectedOps) == 5:
+        time.sleep(1)
+
         conn.flushdb()
+        startTime2 = time.time()
+        Dataloader(conn).generateAndCompressV2(pattern="_SAM_:", commands=commands)
+        endTime2 = time.time()
+        output.append(f"Time taken to ingest compressed data {(endTime2 - startTime2):.3f} seconds")
+        print(f"Time taken to ingest compressed data {(endTime2 - startTime2):.3f} seconds")
+        after = calculate_memory_usage()
+        output.append(f'Memory usage after compression: {after:.2f} MB')
+        print(f'Memory usage after compression: {after:.2f} MB')
 
+        output.append(f'Actual dataset usage of compressed data: {before - after:.2f} MB')
+        output.append(f'Optimization achieved: {(before - after) * 100 / before:.2f}%')
+        print(f'Actual dataset usage of compressed data: {before - after:.2f} MB')
+        print(f'Optimization achieved: {(before - after) * 100 / before:.2f}%')
+
+    # Only loads raw data
+    elif int(selectedOps) == 2:
+        startTime = time.time()
+        Dataloader(conn).generate(pattern="_SAM_:", commands=commands)
+        endTime = time.time()
+        output.append(f"Time taken to ingest raw data {(endTime - startTime):.3f} seconds")
+        print(f"Time taken to ingest raw data {(endTime - startTime):.3f} seconds")
+        before = calculate_memory_usage()
+        output.append(f'Memory usage after loading raw data: {before:.2f} MB')
+        print(f'Memory usage after loading raw data: {before:.2f} MB')
+
+    # Only loads compressed data
+    elif int(selectedOps) == 3:
+        startTime = time.time()
+        Dataloader(conn).generateAndCompressV2(pattern="_SAM_:", commands=commands)
+        endTime = time.time()
+        output.append(f"Time taken to ingest compressed data {(endTime - startTime):.3f} seconds")
+        print(f"Time taken to ingest compressed data {(endTime - startTime):.3f} seconds")
+        after = calculate_memory_usage()
+        output.append(f'Memory usage after compression: {after:.2f} MB')
+        print(f'Memory usage after compression: {after:.2f} MB')
+
+    # Reads raw data (This first generates sample raw data) and records the Time taken
+    elif int(selectedOps) == 4:
+        Dataloader(conn).generate(pattern="_SAM_:", commands=commands)
+        startTime = time.time()
+        DataReader(conn).read(pattern="_SAM_:")
+        endTime = time.time()
+        output.append(f"Time taken to read raw data {(endTime - startTime):.3f} seconds")
+        print(f"Time taken to read raw data {(endTime - startTime):.3f} seconds")
+
+    # Reads compressed data, decodes it (This first generates sample compressed data) and records the Time taken
+    elif int(selectedOps) == 5:
+        Dataloader(conn).generateAndCompressV2(pattern="_SAM_:", commands=commands)
+        startTime = time.time()
+        DataReader(conn).readAndDecompress(pattern="_SAM_:")
+        endTime = time.time()
+        output.append(f"Time taken to read & decode compressed data {(endTime - startTime):.3f} seconds")
+        print(f"Time taken to read & decode compressed data {(endTime - startTime):.3f} seconds")
+
+    with open('output.txt', 'w') as file:
+        for line in output:
+            file.write(line + '\n')
     print("Done!!!")
 
 
